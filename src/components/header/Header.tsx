bd0e245cfc6fdf58108ecb4511c3c23ffc8b4600
ackmanx <ackmanx@users.noreply.github.com>
2022-01-21 21:13:28 -0600

    Added sign in and sign out icons

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,24 @@
 import { signIn, signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import menuIcon from './menu-icon.png'
+import signInIcon from './sign-in.png'
+import signOutIcon from './sign-out.png'
 import { useState } from 'react'
 import styled from '@emotion/styled'
 
 const Header = styled.header`
   display: flex;
-  margin: 0 10px;
+  padding: 10px 20px;
+  background-color: ${(props) => (props.hasActiveToken ? 'none' : '#c00')};
+
+  img {
+    cursor: pointer;
+  }
+`
+const DivImageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
 `
 
 const Component = () => {
@@ -17,14 +29,17 @@ const Component = () => {
 
   return (
     <Header>
-      <Image src={menuIcon} width={50} height={50} onClick={handleOpenMenu} />
+      <Image src={menuIcon} width={60} height={60} onClick={handleOpenMenu} />
 
       {isMenuOpen && (
-        <div>
-          Signed in as {session?.user?.email} <br />
-          <button onClick={() => signOut()}>Sign out</button>
-          <button onClick={() => signIn()}>Sign in again (to get new token)</button>
-        </div>
+        <DivImageContainer>
+          <div>
+            <Image src={signOutIcon} width={40} height={40} onClick={() => signOut()} />
+          </div>
+          <div>
+            <Image src={signInIcon} width={40} height={40} onClick={() => signIn()} />
+          </div>
+        </DivImageContainer>
       )}
     </Header>
   )
