5d9778f7e0e549fa827e9069c8b309b1ccdbcc86
ackmanx <ackmanx@users.noreply.github.com>
2022-01-22 22:24:23 -0600

    Showing refresh button on hover

--- src/components/genres/Genres.tsx
+++ src/components/genres/Genres.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled'
 import { _Artist } from '../../mongoose/Artist'
+import { useState } from 'react'
 import { Image } from '../shared/Image'
 import refreshIcon from './refresh-icon.png'
 
@@ -12,6 +13,18 @@ const Div = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 0 20px;
+
+  &:hover {
+    background-color: #efefef;
+
+    & h2 {
+      color: white;
+    }
+  }
+
+  & img:hover {
+    cursor: pointer;
+  }
 `
 
 const H2 = styled.h2`
@@ -21,14 +34,19 @@ const H2 = styled.h2`
 `
 
 export const Genres = ({ artists }: Props) => {
+  const [showRefreshButton, setShowRefreshButton] = useState(false)
+
   if (!artists.length) {
     return null
   }
 
   return (
-    <Div>
+    <Div
+      onMouseEnter={() => setShowRefreshButton(true)}
+      onMouseLeave={() => setShowRefreshButton(false)}
+    >
       <H2>Industrial Metal</H2>
-      <Image src={refreshIcon} width={40} height={40} />
+      {showRefreshButton && <Image src={refreshIcon} width={40} height={40} />}
     </Div>
   )
 }
