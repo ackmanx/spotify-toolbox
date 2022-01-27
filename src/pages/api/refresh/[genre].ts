61bfd248ecdf06db2a4295a5a877b13814a6efdf
ackmanx <ackmanx@users.noreply.github.com>
2022-01-26 20:34:25 -0600

    Getting all followed artists as array. Added helpers to deal with paging because Spotify Web Api Node library doesn't do that

--- src/pages/api/refresh/[genre].ts
+++ src/pages/api/refresh/[genre].ts
@@ -4,10 +4,15 @@ import { _Artist, Artist } from '../../../mongoose/Artist'
 import { _User, User } from '../../../mongoose/User'
 import { getSession } from 'next-auth/react'
 import { FindOne } from '../../../mongoose/types'
+import { GetAll, getSpotifyWebApi } from '../../../utils/server/spotify-web-api'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   const session = await getSession({ req })
   await dbConnect()
 
-  res.send({ a: req.query.genre ?? 'no genre sent' })
+  const spotify = await getSpotifyWebApi(req)
+  const spotifyResponse = await spotify.getFollowedArtists({ limit: 50 })
+  const allFollowedArtists = await GetAll.followedArtists(spotifyResponse, spotify)
+
+  res.send(allFollowedArtists)
 }
---
+++ src/utils/server/spotify-web-api.ts
@@ -0,0 +1,44 @@
+import SpotifyWebApi from 'spotify-web-api-node'
+import { NextApiRequest } from 'next'
+import { getSession } from 'next-auth/react'
+import ArtistObjectFull = SpotifyApi.ArtistObjectFull
+
+interface SpotifyWebApiResponse<T> {
+  body: T
+  headers: Record<string, string>
+  statusCode: number
+}
+
+export const getSpotifyWebApi = async (req: NextApiRequest) => {
+  const session = await getSession({ req })
+
+  const spotifyWebApi = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  })
+
+  spotifyWebApi.setAccessToken(session?.accessToken as string)
+
+  return spotifyWebApi
+}
+
+export const GetAll = {
+  followedArtists: async (
+    spotifyResponse: SpotifyWebApiResponse<SpotifyApi.UsersFollowedArtistsResponse>,
+    spotifyWebApi: any
+  ) => {
+    const limit = 50
+    const results: ArtistObjectFull[] = []
+    let afterCursor = spotifyResponse.body.artists.cursors.after
+
+    results.push(...spotifyResponse.body.artists.items)
+
+    while (afterCursor) {
+      const response = await spotifyWebApi.getFollowedArtists({ limit, after: afterCursor })
+      afterCursor = response.body.artists.cursors.after
+      results.push(response.body)
+    }
+
+    return results
+  },
+}
