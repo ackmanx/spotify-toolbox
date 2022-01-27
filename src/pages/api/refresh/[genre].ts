66963581a1365c20030ec9da20dce3a18058a454
ackmanx <ackmanx@users.noreply.github.com>
2022-01-27 00:30:02 -0600

    doing stuff

--- src/pages/api/refresh/[genre].ts
+++ src/pages/api/refresh/[genre].ts
@@ -7,9 +7,30 @@ import { FindOne } from '../../../mongoose/types'
 import { GetAll, getSpotifyWebApi } from '../../../utils/server/spotify-web-api'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  const genreToRefresh = req.query.genreToRefresh
   await dbConnect()
 
-  const allFollowedArtists = await GetAll.followedArtists(req)
+  const spotifyFollowedArtists = await GetAll.followedArtists(req)
+  const spotifyFollowedArtistsIDs = spotifyFollowedArtists.map((artist) => artist.id)
 
-  res.send(allFollowedArtists)
+  const artists: _Artist[] = await Artist.find({ artistId: { $in: spotifyFollowedArtistsIDs } })
+  const artistIDs = artists.map((artist) => artist.artistId)
+
+  const artistsNotInDB = spotifyFollowedArtistsIDs.filter(
+    (spotifyArtist) => !artistIDs.includes(spotifyArtist)
+  )
+
+  //todo majerus: not in db means we haven't guessed the genre yet
+  //todo majerus: need to guess, add it to list of artists we already got from db and then save it to the db
+
+  //todo majerus: now we can filter all artists based on genre, and send those back to prove it's correct
+
+  //todo majerus: then we need to get all albums for each artist and filter out seen ones
+  //todo majerus: if there are new ones, run them through the same-name filter
+  //todo majerus: finally, the remaining are what's new
+  //todo majerus: only return the artists that have new albums
+
+  //todo majerus: these steps dont' account for saving album data to artist db
+
+  res.send(artistsNotInDB)
 }
