3f7b06579730c9f5f1e5cf418a65899778f9110e
ackmanx <ackmanx@users.noreply.github.com>
2022-01-30 12:38:56 -0600

    Added link to artist page

--- src/components/artists/Artist.tsx
+++ src/components/artists/Artist.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
 import styled from '@emotion/styled'
 import { _Artist } from '../../mongoose/Artist'
 import ArtistPlaceholder from './artist-placeholder.png'
+import { ImageLink } from '../shared/Image'
 
 interface Props {
   artist: _Artist
@@ -29,7 +29,12 @@ export const Artist = ({ artist }: Props) => {
   return (
     <DivRoot>
       <DivImageContainer>
-        <Image src={artist.coverArt || ArtistPlaceholder} width={300} height={300} />
+        <ImageLink
+          href={`/artist/${artist.id}`}
+          imageSrc={artist.coverArt || ArtistPlaceholder}
+          width={300}
+          height={300}
+        />
       </DivImageContainer>
       <h3>{artist.name}</h3>
     </DivRoot>
--- src/components/shared/Image.tsx
+++ src/components/shared/Image.tsx
@@ -1,16 +1,32 @@
 import NextImage from 'next/image'
 import { ImageProps } from 'next/dist/client/image'
 import styled from '@emotion/styled'
+import Link from 'next/link'
 
 const Button = styled.button`
   background-color: transparent;
   border: none;
 `
 
-type Props = ImageProps & { onClick(event: any): void }
+type ButtonImageProps = ImageProps & { onClick(event: any): void }
 
-export const ButtonImage = ({ onClick, ...props }: Props) => (
+export const ButtonImage = ({ onClick, ...props }: ButtonImageProps) => (
   <Button onClick={onClick}>
     <NextImage {...props} />
   </Button>
+)
+
+interface ImageLinkProps {
+  href: string
+  imageSrc: string | StaticImageData
+  width: number
+  height: number
+}
+
+export const ImageLink = ({ href, imageSrc, width, height }: ImageLinkProps) => (
+  <Link href={href}>
+    <a>
+      <NextImage src={imageSrc} width={width} height={height} />
+    </a>
+  </Link>
 )
