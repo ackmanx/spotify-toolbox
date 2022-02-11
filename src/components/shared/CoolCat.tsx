1275beb852860547926ab3f19731732d6053eba9
ackmanx <ackmanx@users.noreply.github.com>
2022-02-10 19:43:05 -0600

    Cool Cat for messaging

---
+++ src/components/shared/CoolCat.tsx
@@ -0,0 +1,18 @@
+import CoolCatImage from '../images/cool-cat.png'
+import { ButtonImage } from './Image'
+
+interface Props {
+  header: string
+  subheader: string
+  onClick?: () => void
+}
+
+const noop = () => {}
+
+export const CoolCat = ({ onClick = noop, header, subheader }: Props) => (
+  <div>
+    <h2>{header}</h2>
+    <h2>{subheader}</h2>
+    <ButtonImage src={CoolCatImage} onClick={onClick} />
+  </div>
+)
