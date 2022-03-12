36780a71c5c998418e8ff06b67e6cdfa39de58f4
ackmanx <ackmanx@users.noreply.github.com>
2022-03-12 17:58:13 -0600

    comments

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,10 +1,11 @@
-// This is a type guard to allow you to use `.filter(Boolean)`
 import { _ViewedAlbums } from '../mongoose/User'
 
+// This is a type guard to allow you to use `.filter(Boolean)`
 export function filterNonNull<T>(value: T | null | undefined): value is T {
   return value != null
 }
 
+// This is usually used to avoid trying to save something to the DB that already exists when doing batch saving
 export function removeDuplicates(source: any[], overlap: string[]) {
   return source.filter((item) => !overlap.includes(item.id))
 }
