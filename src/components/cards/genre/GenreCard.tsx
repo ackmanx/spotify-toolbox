3ee8735a735a4507ed29c756dff0f499b74caaa2
ackmanx <ackmanx@users.noreply.github.com>
2022-04-06 00:20:30 -0500

    Switching to genre cover art from artists instead of icons

--- src/components/cards/genre/GenreCard.tsx
+++ src/components/cards/genre/GenreCard.tsx
@@ -14,10 +14,38 @@ const styles = {
     display: inline-block;
     width: 200px;
     height: 200px;
-    margin: 20px;
+    margin: 30px;
   `,
-  top: css`
-    margin-bottom: -5px;
+  topLeft: css`
+    display: inline-block;
+
+    img {
+      border-top-left-radius: 5px;
+    }
+  `,
+  topRight: css`
+    display: inline-block;
+
+    img {
+      border-top-right-radius: 5px;
+    }
+  `,
+  bottomLeft: css`
+    display: inline-block;
+
+    img {
+      border-bottom-left-radius: 5px;
+    }
+  `,
+  bottomRight: css`
+    display: inline-block;
+
+    img {
+      border-bottom-right-radius: 5px;
+    }
+  `,
+  name: css`
+    margin: 0;
   `,
 }
 
@@ -25,13 +53,15 @@ const coverArtSize = 100
 
 export const GenreCard = ({ genreName, coverArts }: Props) => (
   <div css={styles.root}>
-    <div css={styles.top}>
+    <div css={styles.topLeft}>
       <ImageLink
         href={`/app/genre/${genreName}`}
         imageSrc={coverArts[0]}
         width={coverArtSize}
         height={coverArtSize}
       />
+    </div>
+    <div css={styles.topRight}>
       <ImageLink
         href={`/app/genre/${genreName}`}
         imageSrc={coverArts[1]}
@@ -39,13 +69,15 @@ export const GenreCard = ({ genreName, coverArts }: Props) => (
         height={coverArtSize}
       />
     </div>
-    <div>
+    <div css={styles.bottomLeft}>
       <ImageLink
         href={`/app/genre/${genreName}`}
         imageSrc={coverArts[2]}
         width={coverArtSize}
         height={coverArtSize}
       />
+    </div>
+    <div css={styles.bottomRight}>
       <ImageLink
         href={`/app/genre/${genreName}`}
         imageSrc={coverArts[3]}
@@ -53,6 +85,6 @@ export const GenreCard = ({ genreName, coverArts }: Props) => (
         height={coverArtSize}
       />
     </div>
-    <h3>{genreName}</h3>
+    <h3 css={styles.name}>{genreName}</h3>
   </div>
 )
