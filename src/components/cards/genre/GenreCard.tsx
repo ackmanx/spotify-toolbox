4d0396492a5d8554a6d95543f2b82e9351946cfa
ackmanx <ackmanx@users.noreply.github.com>
2022-04-05 23:18:41 -0500

    Switching to genre cover art from artists instead of icons

--- src/components/cards/genre/GenreCard.tsx
+++ src/components/cards/genre/GenreCard.tsx
@@ -10,19 +10,49 @@ interface Props {
 
 const styles = {
   root: css`
+    // To allow each genre card to autowrap on the page
     display: inline-block;
-    width: 300px;
-    height: 300px;
+    width: 200px;
+    height: 200px;
     margin: 20px;
+  `,
+  top: css`
+    margin-bottom: -5px;
   `,
 }
 
+const coverArtSize = 100
+
 export const GenreCard = ({ genreName, coverArts }: Props) => (
   <div css={styles.root}>
-    <ImageLink href={`/app/genre/${genreName}`} imageSrc={coverArts[0]} width={150} height={150} />
-    <ImageLink href={`/app/genre/${genreName}`} imageSrc={coverArts[1]} width={150} height={150} />
-    <ImageLink href={`/app/genre/${genreName}`} imageSrc={coverArts[2]} width={150} height={150} />
-    <ImageLink href={`/app/genre/${genreName}`} imageSrc={coverArts[3]} width={150} height={150} />
+    <div css={styles.top}>
+      <ImageLink
+        href={`/app/genre/${genreName}`}
+        imageSrc={coverArts[0]}
+        width={coverArtSize}
+        height={coverArtSize}
+      />
+      <ImageLink
+        href={`/app/genre/${genreName}`}
+        imageSrc={coverArts[1]}
+        width={coverArtSize}
+        height={coverArtSize}
+      />
+    </div>
+    <div>
+      <ImageLink
+        href={`/app/genre/${genreName}`}
+        imageSrc={coverArts[2]}
+        width={coverArtSize}
+        height={coverArtSize}
+      />
+      <ImageLink
+        href={`/app/genre/${genreName}`}
+        imageSrc={coverArts[3]}
+        width={coverArtSize}
+        height={coverArtSize}
+      />
+    </div>
     <h3>{genreName}</h3>
   </div>
 )
